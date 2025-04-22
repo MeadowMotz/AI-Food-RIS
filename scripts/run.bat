a7ca787c4d892cec11c1102e5f44458840aa5b17
Meadow Motz <motzb1@udayton.edu>
2025-04-22 09:44:53 -0400

    Remove drag & drop

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,6 +1,6 @@
-py -3.10 -m pip install -r ../requirements.txt
-py -3.10 download_dataset.py
-py -3.10 preprocess.py
-py -3.10 extract_features.py
-py -3.10 build_index.py
+pip install -r ../requirements.txt
+python download_dataset.py
+python preprocess.py
+python extract_features.py
+python build_index.py
 pause
